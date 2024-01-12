39ae37ef0791bf1945401beda55fe1329c660768
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-01-12 14:41:17 +0100

    refactor(builtin_utils.c): remove print_data_builtins function to clean up code and improve readability This function was used for debugging and is no longer needed.

--- src/execution/builtin_utils.c
+++ src/execution/builtin_utils.c
@@ -6,30 +6,13 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:10:43 by ladloff           #+#    #+#             */
-/*   Updated: 2024/01/11 19:25:48 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/01/12 14:40:44 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "minishell.h"
 #include "libft.h"
-
-void	print_data_builtins(t_exec	*current)
-{
-	int	i;
-
-	i = 0;
-	printf("------ARGUMENTS ENVOYES EN EXECUTION-----------\n");
-	printf("| %-15s | %-25s |\n", "Int ARGC", "Char **ARGV");
-	printf("-----------------------------------------------\n");
-	printf("| %-15d | %-25s |\n", current->argc, current->argv[i++]);
-	while (current->argv[i])
-	{
-		printf("| %-15s | %-25s |\n", " ", current->argv[i]);
-		i++;
-	}
-	printf("-----------------------------------------------\n\n\n");
-}
 
 void	handle_error_cases(t_exec *exec)
 {
