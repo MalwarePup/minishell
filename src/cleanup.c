2636df065a7c1d4351278ebdd37503c1fc848bec
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-02-12 16:59:34 +0100

    fix(cleanup.c): set exec->pathname to NULL after freeing to prevent dangling pointer issues

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 18:18:59 by ladloff           #+#    #+#             */
-/*   Updated: 2024/02/12 16:50:08 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/02/12 16:59:00 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	cleanup_executable(t_master *master)
 {
 	free_string_array(master->exec->argv);
 	free(master->exec->pathname);
+	master->exec->pathname = NULL;
 }
 
 void	cleanup_before_exit(t_master *master)
