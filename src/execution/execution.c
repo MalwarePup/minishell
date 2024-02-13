344f9fca35d0405cfaf42d52df34bf692b164542
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-02-13 16:02:47 +0100

    fix(execution.c): modify condition to handle CMD_EXPORT in addition to CMD_CD to be working with and without pipe

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 21:20:24 by ladloff           #+#    #+#             */
-/*   Updated: 2024/02/13 15:18:09 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/02/13 15:52:39 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ static t_cmd_type	prepare_execution(t_master *master, t_token *token)
 	launch_expansion(master);
 	update_executable_path(master->exec, master->env_list);
 	type = execute_command_or_builtin(master);
-	if (type == CMD_ERROR || (type == CMD_CD && !token->next))
+	if (type == CMD_ERROR || (!token->next && (type == CMD_CD || type == CMD_EXPORT)))
 	{
-		execute_builtin(master, type);
+		master->exit_status = execute_builtin(master, type);
 		return (CMD_ERROR);
 	}
 	if (token->next && token->next->type == CMD_PIPE)
