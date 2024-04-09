83cad9e25afeb66025ffab4124507e44fbaaed38
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-04-09 10:11:38 +0200

    refactor(ft_cd.c): extract target path determination logic to a separate function for better readability and maintainability fix(ft_cd.c): handle case when HOME or OLDPWD is not set, return 1 instead of trying to change directory style(ft_cd.c): adjust indentation and spacing for consistency and readability

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 11:10:38 by ladloff           #+#    #+#             */
-/*   Updated: 2024/04/07 15:24:55 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/04/09 10:11:05 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "minishell.h"
 #include "libft.h"
 
-static int modify_env_var(t_env_list *env, const char *name, const char *value)
+static int	modify_env_var(t_env_list *env, const char *name, const char *value)
 {
 	t_env_list	*current;
 
@@ -78,42 +78,40 @@ static int	change_directory_and_update(t_master *master, const char *path)
 	return (0);
 }
 
+const char	*determine_target_path(int argc, char **argv, t_master *master)
+{
+	const char	*target_path;
+
+	if (argc != 2 || ft_strncmp(argv[1], "--", 3) == 0)
+	{
+		target_path = getenv_no_alloc(master->env, "HOME");
+		if (!target_path)
+			ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
+	}
+	else if (ft_strncmp(argv[1], "-", 2) == 0)
+	{
+		target_path = getenv_no_alloc(master->env, "OLDPWD");
+		if (!target_path)
+			ft_putstr_fd("minishell: cd: OLDPWD not set\n", STDERR_FILENO);
+		else
+			ft_putendl_fd((char *)target_path, STDOUT_FILENO);
+	}
+	else
+		target_path = argv[1];
+	return (target_path);
+}
+
 int	ft_cd(int argc, char **argv, t_master *master)
 {
-	const char	*home_path;
+	const char	*target_path;
 
 	if (argc > 2)
 	{
 		ft_putstr_fd(ESTR_CD_TOO_MANY_ARGS, STDERR_FILENO);
 		return (1);
 	}
-	else if (argc != 2 || ft_strncmp(argv[1], "--", 3) == 0)
-	{
-		home_path = getenv_no_alloc(master->env, "HOME");
-		if (!home_path)
-		{
-			ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
-			return (1);
-		}
-		return (change_directory_and_update(master, home_path));
-	}
-	else if (ft_strncmp(argv[1], "-", 2) == 0)
-	{
-		home_path = getenv_no_alloc(master->env, "OLDPWD");
-		if (!home_path)
-		{
-			ft_putstr_fd("minishell: cd: OLDPWD not set\n", STDERR_FILENO);
-			return (1);
-		}
-		ft_putendl_fd((char *)home_path, STDOUT_FILENO);
-		return (change_directory_and_update(master, home_path));
-	}
-	else if (ft_strncmp(argv[1], "---", 4) == 0)
-	{
-		ft_putstr_fd("minishell: usage: cd [-L|[-P [-e]] [-@]] [dir]\n",
-			STDERR_FILENO);
-		return (EXIT_MISUSE);
-	}
-	else
-		return (change_directory_and_update(master, (const char *)argv[1]));
+	target_path = determine_target_path(argc, argv, master);
+	if (!target_path)
+		return (1);
+	return (change_directory_and_update(master, target_path));
 }
