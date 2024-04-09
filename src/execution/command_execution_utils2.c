c505869e3bc94997cd121973907dac1e8e3d0a4d
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-04-09 09:44:07 +0200

    fix(command_execution_utils2.c): modify file access error handling to check for redirection This change ensures that the error message for file not found is only printed when there is no redirection. This prevents unnecessary error messages from being displayed.

--- src/execution/command_execution_utils2.c
+++ src/execution/command_execution_utils2.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 10:42:44 by ladloff           #+#    #+#             */
-/*   Updated: 2024/04/09 07:47:46 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/04/09 09:41:28 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,14 +59,18 @@ static bool	handle_file_access_and_errors(t_master *master)
 		&& (!ft_strncmp(master->argv[0], "./", 2)
 			|| !ft_strncmp(master->argv[0], "/", 1)))
 	{
-		ft_dprintf(STDERR_FILENO, ESTR_NO_FILE, master->argv[0]);
-		return (master->exit_status = EXIT_NOT_FOUND, false);
+		if (!master->token || !master->token->redir)
+		{
+			ft_dprintf(STDERR_FILENO, ESTR_NO_FILE, master->argv[0]);
+			return (master->exit_status = EXIT_NOT_FOUND, false);
+		}
 	}
 	else
 	{
 		ft_dprintf(STDERR_FILENO, ESTR_CMD_NOT_FOUND, master->argv[0]);
 		return (master->exit_status = EXIT_NOT_FOUND, false);
 	}
+	return (true);
 }
 
 bool	handle_command_not_found_error(t_master *master)
