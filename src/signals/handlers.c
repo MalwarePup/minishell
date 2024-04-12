8adad4a1fdc23c62b7a8c21bd44975687c9b8b42
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-04-12 23:26:06 +0200

    feat: modify exit status in signal handlers to follow Unix convention

--- src/signals/handlers.c
+++ src/signals/handlers.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 12:59:55 by ladloff           #+#    #+#             */
-/*   Updated: 2024/04/12 23:21:50 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/04/12 23:23:52 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	default_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		*g_exit_status = INTERRUPTED;
+		*g_exit_status = 128 + signum;
 		rl_replace_line("", 0);
 		write(STDOUT_FILENO, "^C\n", 3);
 		rl_on_new_line();
@@ -31,7 +31,7 @@ void	heredoc_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		*g_exit_status = INTERRUPTED_HEREDOC;
+		*g_exit_status = 128 + signum;
 		write(STDOUT_FILENO, "^C\n", 3);
 		close(STDIN_FILENO);
 	}
@@ -41,7 +41,7 @@ void	exec_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		*g_exit_status = INTERRUPTED_TEMP;
+		*g_exit_status = 128 + signum;
 		rl_replace_line("", 0);
 		write(STDOUT_FILENO, "\n", 1);
 		rl_on_new_line();
