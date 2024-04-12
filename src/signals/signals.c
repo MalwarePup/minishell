17b4627325a881073d24ba7b5720d337c5aa0da1
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-04-12 23:17:19 +0200

    refactor: merge initialize_signal_handlers into set_sigaction

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 20:46:13 by ladloff           #+#    #+#             */
-/*   Updated: 2024/04/12 23:11:28 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/04/12 23:16:27 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,14 @@
 #include <stdlib.h>
 #include "minishell.h"
 
-static int	initialize_signal_handlers(t_master *master)
+int	set_sigaction(t_master *master)
 {
 	master->act.sa_handler = handle_minishell_sig;
 	if (sigemptyset(&master->act.sa_mask) == -1)
 	{
-		perror("sigemptyset (initialize_signal_handlers)");
+		perror("sigemptyset (set_sigaction)");
 		return (1);
 	}
-	return (0);
-}
-
-int	set_sigaction(t_master *master)
-{
-	if (initialize_signal_handlers(master))
-		return (1);
 	if (sigaction(SIGINT, &master->act, NULL) == -1
 		|| sigaction(SIGQUIT, &master->act, NULL) == -1)
 	{
